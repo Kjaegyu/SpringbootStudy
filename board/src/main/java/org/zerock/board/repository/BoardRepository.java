f60b7762e8a8038e9d753e2715817f0b6cc4a124
kjg <o_opop9999@naver.com>
2023-08-03 11:34:56 +0900

    관계 처리 에러해결 완료
    이유는 Repository 임포트가 잘못 선언되었다 .. 해결완료 ㅠ

--- board/src/main/java/org/zerock/board/repository/BoardRepository.java
+++ board/src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -1,13 +1,12 @@
 package org.zerock.board.repository;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.board.entity.Board;
 
-import java.awt.print.Pageable;
-import java.nio.file.attribute.GroupPrincipal;
 import java.util.List;
 
 public interface BoardRepository extends JpaRepository<Board, Long> {
@@ -19,13 +18,15 @@ Object getBoardWithWriter(@Param("bno") Long bno);
     @Query("SELECT b, r FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
 
-    @Query(value ="SELECT b, w, count(r)" +
-    "FROM Board b" +
-    "LEFT JOIN b.writer w " +
-    "LEFT JOIN Reply r ON r.board = b " +
-            "GROUP BY b",
+    @Query(value ="SELECT b, w, count(r) " +
+    " FROM Board b " +
+    " LEFT JOIN b.writer w " +
+    " LEFT JOIN Reply r ON r.board = b " +
+            " GROUP BY b ",
     countQuery = "SELECT count(b) FROM Board b")
-    Page<Object> getBoardWithReplyCount(Pageable pageable); //목록 화면에 필요한 데이터
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable); //목록 화면에 필요한 데이터
+
+    
 
 
 }
