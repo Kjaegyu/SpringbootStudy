da586086c5b1db205627bb7026511c68fd5516e2
kjg <o_opop9999@naver.com>
2023-08-04 10:15:22 +0900

    게시물 등록 처리

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -5,8 +5,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.board.dto.BoardDTO;
 import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.entity.Board;
 import org.zerock.board.service.BoardService;
 
 @Controller
@@ -23,6 +28,26 @@ public class BoardController {
 
         model.addAttribute("result", boardService.getList(pageRequestDTO));
 
+
+    }
+
+    @GetMapping("/register")
+    public void register(){
+        log.info("regiser get...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(BoardDTO dto, RedirectAttributes redirectAttributes){
+
+        log.info("dto..." + dto);
+        //새로 추가된 엔티티의 번호
+        Long bno = boardService.register(dto);
+
+        log.info("BNO: " + bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
     }
 
 }
