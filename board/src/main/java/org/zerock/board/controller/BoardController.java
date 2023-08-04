8d22a860d47435806338c2ce9d37f7276697fe8b
kjg <o_opop9999@naver.com>
2023-08-04 12:06:30 +0900

    Querydsl 설정
    설정만 1시간걸림..;

--- board/src/main/java/org/zerock/board/controller/BoardController.java
+++ board/src/main/java/org/zerock/board/controller/BoardController.java
@@ -2,6 +2,7 @@ package org.zerock.board.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -51,7 +52,7 @@ public class BoardController {
         return "redirect:/board/list";
     }
 
-    @GetMapping("/read")
+    @GetMapping({"/read", "/modify"})
     public void read(@ModelAttribute("requestDTO") PageRequestDTO pageRequestDTO, Long bno, Model model){
 
         log.info("bno: " + bno);
@@ -63,4 +64,33 @@ public class BoardController {
         model.addAttribute("dto", boardDTO);
 
     }
+
+    @PostMapping("/remove")
+    public String remove(long bno, RedirectAttributes redirectAttributes) {
+
+        log.info("bno: " + bno);
+
+        boardService.removeWithReplies(bno);
+
+        redirectAttributes.addFlashAttribute("msg", bno);
+
+        return "redirect:/board/list";
+    }
+    @PostMapping("/modify")
+    public String modify(BoardDTO dto, @ModelAttribute("requestDTO")PageRequestDTO requestDTO, RedirectAttributes redirectAttributes) {
+
+        log.info("post modify..........................................");
+        log.info("dto: " + dto);
+
+        boardService.modify(dto);
+
+        redirectAttributes.addAttribute("page",requestDTO.getPage());
+        redirectAttributes.addAttribute("type",requestDTO.getType());
+        redirectAttributes.addAttribute("keyword",requestDTO.getKeyword());
+
+        redirectAttributes.addAttribute("bno",dto.getBno());
+
+        return "redirect:/board/read";
+    }
+
 }
